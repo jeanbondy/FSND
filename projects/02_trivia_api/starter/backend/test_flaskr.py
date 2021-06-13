737b6acb03dbddec854abcb7dc923f95dae1074a
boje <jbondy@icloud.com>
2021-06-14 01:17:19 +0200

    WIP unittests

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -2,7 +2,6 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
 from flaskr import create_app
 from models import setup_db, Question, Category
 
@@ -24,9 +23,23 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
+
+        self.new_question = {
+            'question': 'What time is it',
+            'answer': 'too late',
+            'category': 1,
+            'difficulty': 1
+        }
+
+        self.search_term = {'searchTerm': 'hank'}
+
+        self.quizzes = {'previous_questions': [13,14],
+                        'quiz_category': {'type': 'click',
+                                          'id': 0}
+                        }
     
     def tearDown(self):
-        """Executed after reach test"""
+        """Executed after each test"""
         pass
 
     """
@@ -34,7 +47,113 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_quizzes(self):
+        res = self.client().post('/quizzes', json=self.quizzes)
+        data = json.loads(res.data)
+        print(f'res data is {data}')
 
-# Make the tests conveniently executable
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+
+    def test_400_quizzes_malformed(self):
+        res = self.client().post('/quizzes')
+        data = json.loads(res.data)
+        print(f'res data is {data}')
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+
+
+# working tests
+'''
+        def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(data['currentCategory'])
+
+    def test_404_get_paginated_questions_beyond_valid_page(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    def test_post_new_question(self):
+        res = self.client().post('/questions', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_400_post_new_question_malformed(self):
+        res = self.client().post('/questions', json={})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+
+
+    def test_search_question(self):
+        res = self.client().post('/questions', json=self.search_term)
+        data = json.loads(res.data)
+
+        print(res.status_code)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['totalQuestions'])
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/11')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'resource deleted')
+        
+    def test_404_delete_missing_question(self):
+        res = self.client().delete('/questions/11')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    def test_questions_by_category(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['currentCategory'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['totalQuestions'])
+
+    def test_404_questions_by_category(self):
+        res = self.client().get('/categories/95/questions')
+        data = json.loads(res.data)
+
+        print(f'res data is {data}')
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+        
+
+
+'''
